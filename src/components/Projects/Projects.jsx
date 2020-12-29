737ba73ffdac24681ed3fdb5ede9ca4b229a7469
Fernanda Moreno <fmoreno6@gatech.edu>
2020-12-28 19:43:15 -0500

    remove links from project images

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -82,8 +82,6 @@ const Projects = () => {
                   >
                     <div className="project-wrapper__image">
                       <a
-                        href={url || '#!'}
-                        target="_blank"
                         aria-label="Project Link"
                         rel="noopener noreferrer"
                       >
